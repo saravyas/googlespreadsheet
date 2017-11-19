fdd4ebe2f15771c1336fb03ca075de6a44afe67d
saravyas <saravyas007@gmail.com>
2017-11-19 11:29:25 +0530

    googlespreadsheet connected with nodejs

---
+++ nodespreadsheet/spreadsheet.js
@@ -0,0 +1,13 @@
+var Gs = require('google-spreadsheet');
+var file = require('./client_config.json');
+var doc = new Gs('1PWaUJ-uwQFmFNzW1JwxsREwyCh6rLrBHjrZnanMtbnM');
+
+doc.useServiceAccountAuth(file, function (err) {
+ 
+  doc.getRows(1, function (err, rows) {
+  	rows[2].del();
+    console.log(rows);
+  });
+});
+
+ 
