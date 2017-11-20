4e8d25f4e312eb8a1be791604e53b433b345544b
saravyas <saravyas007@gmail.com>
2017-11-20 09:41:37 +0530

    added code for update and delete in spreadsheet

--- pythonspreadsheet/spread.py
+++ pythonspreadsheet/spread.py
@@ -8,8 +8,23 @@ client = gspread.authorize(creds)
 
 sheet = client.open('data').sheet1
 
-# pp = pprint.PrettyPrinter()
+pp = pprint.PrettyPrinter()
 
-sara = sheet.get_all_records()
+# sara = sheet.get_all_records()
 
-print(sara) 		
+# sara = sheet.col_values(2)
+
+#sara = sheet.cell(2,3).value
+
+# sheet.update_cell(3,3,"gomalvyas007@gmail.com")
+# sara = sheet.cell(3,3).value
+# row = ["haseeb",21,"haseeb@gmail.com"]
+# index = 4
+
+# sara = sheet.insert_row(row,index) 
+
+
+# sheet.delete_row(4)
+
+print(sheet.row_count)
+#pp.pprint(sara) 		
