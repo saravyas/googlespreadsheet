5bd5694b3c10b1cab4c13ddd7efc8bafe00e95b3
Saravyas <saravyas007@gmail.com>
2018-11-30 15:48:35 -0500

    config change

--- nodespreadsheet/spreadsheet.js
+++ nodespreadsheet/spreadsheet.js
@@ -1,5 +1,6 @@
 var Gs = require('google-spreadsheet');
 var file = require('./client_config.json');
+// Please load your config file
 var doc = new Gs('1PWaUJ-uwQFmFNzW1JwxsREwyCh6rLrBHjrZnanMtbnM');
 
 doc.useServiceAccountAuth(file, function (err) {
